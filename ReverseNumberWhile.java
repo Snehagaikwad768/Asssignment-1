67b76e0278eb6867ed0f3f1f0aa2065c88e681a3
Snehagaikwad768 <109944120+Snehagaikwad768@users.noreply.github.com>
2022-07-25 01:15:07 -0700

    Add files via upload

---
+++ ReverseNumberWhile.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Lenovo
+ */
+import java.util.Scanner;
+public class ReverseNumberWhile {
+    
+    public static void main(String args[])
+   {
+      int num=0;
+      int reversenum =0;
+      System.out.println("Input your number and press enter: ");
+      //This statement will capture the user input
+      Scanner in = new Scanner(System.in);
+      //Captured input would be stored in number num
+      num = in.nextInt();
+      //While Loop: Logic to find out the reverse number
+      while( num != 0 )
+      {
+          reversenum = reversenum * 10;
+          reversenum = reversenum + num%10;
+          num = num/10;
+      }
+
+      System.out.println("Reverse of input number is: "+reversenum);
+   }
+}
+
