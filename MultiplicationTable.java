0c6c58897ff124828596645001b4a6bbeeeb189a
Snehagaikwad768 <109944120+Snehagaikwad768@users.noreply.github.com>
2022-07-25 00:50:30 -0700

    Program For Multiplication Table

---
+++ MultiplicationTable.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+import java.util.Scanner;
+public class MultiplicationTable {
+    public static void main(String[] args) {
+
+        int num = 5;
+        for(int i = 1; i <= 10; ++i)
+        {
+            System.out.printf("%d * %d = %d \n", num, i, num * i);
+        }
+    }
+}
+    
+
