d31c3b1943565ebccc832865b7af7e677969ecdb
Snehagaikwad768 <109944120+Snehagaikwad768@users.noreply.github.com>
2022-07-25 08:23:05 -0700

    pro to Find  Quotient and Remainder

---
+++ Quotientremainder.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Quotientremainder {
+      //Java Program to find the quotient and remainder
+  public static void main(String[] args) 
+    {
+        int num1 = 19, num2 = 4;  //Declare and initialize the numbers
+        System.out.println("The entered number is: "+num1);
+        System.out.println("The entered number is: "+num1);
+        int quotient = num1 / num2;   //Find quotient
+        int remainder = num1 % num2;  //Find Remainnder
+        System.out.println("After division the quotient and remainder are: ");
+        //Print the quotient and remainder
+        System.out.println("The quotient is: " + quotient);
+        System.out.println("The remainder is: " + remainder);
+    }
+}
+
