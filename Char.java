01360382387424b108f361a4de78483ea8aee720
Snehagaikwad768 <109944120+Snehagaikwad768@users.noreply.github.com>
2022-07-25 01:27:30 -0700

    Add files via upload

---
+++ Char.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+import java.util.Scanner;
+public class Char {
+    public static void main(String[ ] arg)
+	{
+	int i=0;
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter a character : ");
+	char ch=sc.next( ).charAt(0);	 		
+	if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+	{
+	System.out.println("Entered character "+ch+" is  Vowel"); 
+	}
+	else if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+		System.out.println("Entered character "+ch+" is Consonant");
+	      else
+		System.out.println("Not an alphabet");		
+	}
+}
+    
+
