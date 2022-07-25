764a753bca4774fb0f312e398f6cc4640b5143aa
Snehagaikwad768 <109944120+Snehagaikwad768@users.noreply.github.com>
2022-07-25 08:39:29 -0700

    Add files via upload

---
+++ LeapYear.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+import java.util.Scanner;
+public class LeapYear {
+    public static void main(String[] args){
+      int year;
+      System.out.println("Enter an Year :: ");
+      Scanner sc = new Scanner(System.in);
+      year = sc.nextInt();
+       if (year % 4 ==0 )
+         System.out.println("Specified year is a leap year");
+      else
+         System.out.println("Specified year is not a leap year");
+   }
+
+    
+}
