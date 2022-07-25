47d9c196724da0c41553b7da76a5a2ab208b21f6
Snehagaikwad768 <109944120+Snehagaikwad768@users.noreply.github.com>
2022-07-25 10:44:25 -0700

    Add files via upload

---
+++ Armstrong.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Armstrong {
+    public static void main(String[] args) {
+
+        int num = 153, number, temp, total = 0;
+        
+
+        number = num;
+        while (number != 0)
+        {
+            temp = number % 10;
+            total = total + temp*temp*temp;
+            number /= 10;
+        }
+
+        if(total == num)
+            System.out.println(num + " is an Armstrong number");
+        else
+            System.out.println(num + " is not an Armstrong number");
+    }
+    
+}
+ 
+
