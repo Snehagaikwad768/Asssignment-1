382324773121c40a965fef8950fe83d8f2ff9d78
Snehagaikwad768 <109944120+Snehagaikwad768@users.noreply.github.com>
2022-07-25 22:59:59 -0700

    Add files via upload

---
+++ TestStringConcatenation1.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public class TestStringConcatenation1 {
+    public static void main(String args[]){  
+   String s="Sachin"+" Tendulkar";  
+   System.out.println(s);//Sachin Tendulkar  
+ }  
+}
+
